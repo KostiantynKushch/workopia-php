9a9149e201542de8fb34e1440c70eb57d1453e25
kostiantyn.kushch <kostiantyn.kushch@gmail.com>
2024-11-14 13:57:00 +0200

    split UI into partials

--- helpers.php
+++ helpers.php
@@ -10,3 +10,35 @@ function basePath($path = '')
 {
   return __DIR__ . '/' . $path;
 }
+
+/**
+ * Load a view
+ * 
+ * @param string $name
+ * @return void
+ */
+function loadView($name)
+{
+  $path = basePath("views/{$name}.view.php");
+  if (file_exists($path)) {
+    require $path;
+  } else {
+    echo "View '{$name}' not found! Please ensure the file exists at the specified path: '{$path}'.";
+  }
+}
+
+/**
+ * Load a partial
+ * 
+ * @param string $name
+ * @return void
+ */
+function loadPartial($name)
+{
+  $path = basePath("views/partials/{$name}.php");
+  if (file_exists($path)) {
+    require $path;
+  } else {
+    echo "Partial '{$name}' not found! Please ensure the file exists at the specified path: '{$path}'.";
+  }
+}
