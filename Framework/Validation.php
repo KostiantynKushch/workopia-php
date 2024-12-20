515401d7936691f4d3f1d049c2ba0f348daa533a
kostiantyn.kushch <kostiantyn.kushch@gmail.com>
2024-12-20 14:33:31 +0200

    add Validation class

---
+++ Framework/Validation.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Framework;
+
+class Validation
+{
+
+  /**
+   * Validate a string
+   *
+   * @param string $value
+   * @param int $min
+   * @param int $max
+   * @return bool
+   */
+  public static function string($value, $min = 1, $max = INF)
+  {
+    if (is_string($value)) {
+      $length = strlen(trim($value));
+      return $length >= $min && $length < $max;
+    }
+
+    return false;
+  }
+
+
+  /**
+   * Validate email
+   *
+   * @param string $value
+   * @return mixed
+   */
+  public static function email($value)
+  {
+    return filter_var(trim($value), FILTER_VALIDATE_EMAIL);
+  }
+
+
+  /**
+   * Match a value against another
+   *
+   * @param string $value1
+   * @param string $value2
+   * @return bool
+   */
+  public static function match($value1, $value2)
+  {
+    return trim($value1) === trim($value2);
+  }
+}
